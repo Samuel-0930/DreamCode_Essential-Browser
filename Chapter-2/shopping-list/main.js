3c6d1c3852e80172647cccc646b7943ecc0f2da0
Samuel Son <samyeol0930@gmail.com>
2023-09-13 10:58:47 +0900

    add function

---
+++ Chapter-2/shopping-list/main.js
@@ -0,0 +1,25 @@
+const input = document.querySelector('input');
+const add = document.querySelector('.plus');
+const lists = document.querySelector('.lists');
+const list = document.querySelector('.list');
+
+function add_context() {
+	const list = document.createElement('li');
+	const image = document.createElement('img');
+	list.setAttribute('class', 'list');
+	image.setAttribute('class', 'list__image--delete');
+	image.src = 'img/trash can.png';
+	list.textContent = input.value;
+	lists.appendChild(list);
+	list.appendChild(image);
+	input.value = null;
+}
+function show_name(e) {
+	if (e.keyCode == 13) {
+		add_context();
+	}
+}
+
+function add_value() {
+	add_context();
+}
