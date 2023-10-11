9cad03900538ed862611eba386a36e964a5311d8
Samuel Son <samyeol0930@gmail.com>
2023-10-11 15:31:12 +0900

    javascript done

--- Chapter-2/carrot_elle/main.js
+++ Chapter-2/carrot_elle/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const CARROT_SIZE = 80;
-const CARROT_COUNT = 5;
-const BUG_COUNT = 5;
-const GAME_DURATION_SEC = 5;
+const CARROT_COUNT = 20;
+const BUG_COUNT = 20;
+const GAME_DURATION_SEC = 20;
 
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
@@ -13,6 +13,12 @@ const gameScore = document.querySelector('.game__score');
 const popUp = document.querySelector('.pop-up');
 const popUpText = document.querySelector('.pop-up__message');
 const popUpRefresh = document.querySelector('.pop-up__refresh');
+
+const carrotSound = new Audio('./sound/carrot_pull.mp3');
+const alertSound = new Audio('./sound/alert.wav');
+const bgSound = new Audio('./sound/bg.mp3');
+const bugSound = new Audio('./sound/bug_pull.mp3');
+const winSound = new Audio('./sound/game_win.mp3');
 
 let started = false;
 let score = 0;
@@ -49,6 +55,7 @@ function startGame() {
 	showStopButton();
 	showTimerAndScore();
 	startGameTimer();
+	playSound(bgSound);
 }
 
 function stopGame() {
@@ -56,13 +63,22 @@ function stopGame() {
 	clearInterval(timer);
 	stopGameTimer();
 	hideGameButton();
+	playSound(alertSound);
 	showPopUpWithText('REPLAY?');
+	stopSound(bgSound);
 }
 
 function finishGame(win) {
 	started = false;
 	clearInterval(timer);
 	hideGameButton();
+	stopGameTimer();
+	stopSound(bgSound);
+	if (win) {
+		playSound(winSound);
+	} else {
+		playSound(bugSound);
+	}
 	showPopUpWithText(win ? 'YOU WIN!!' : 'YOU LOSE@@');
 }
 
@@ -70,6 +86,7 @@ function showStopButton() {
 	const icon = gameBtn.querySelector('.fa-solid');
 	icon.classList.add('fa-stop');
 	icon.classList.remove('fa-play');
+	gameBtn.style.visibility = 'visible';
 }
 
 function hideGameButton() {
@@ -122,15 +139,25 @@ function onFieldClick(event) {
 		//당근!!
 		target.remove();
 		score++;
+		playSound(carrotSound);
 		updateScoreBoard();
 		if (score === CARROT_COUNT) {
 			finishGame(true);
 		}
 	} else if (target.matches('.bug')) {
 		//벌레!!
-		stopGameTimer();
+
 		finishGame(false);
 	}
+}
+
+function playSound(sound) {
+	sound.currentTime = 0;
+	sound.play();
+}
+
+function stopSound(sound) {
+	sound.pause();
 }
 
 function updateScoreBoard() {
